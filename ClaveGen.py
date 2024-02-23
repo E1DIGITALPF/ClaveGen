e3a10b9abcce985e9df10f28c54a27a681b9d6f2
Eliécer Hernández <paraguanads@dexkit.com>
2024-02-23 07:57:23 -0400

    Misc

--- ClaveGen.py
+++ ClaveGen.py
@@ -1,13 +1,15 @@
 import random
 import tkinter as tk
-from random import randint
 from string import ascii_letters, digits, punctuation
 from tkinter import ttk, messagebox
 
 def generar_contrasena():
-    longitud = randint(12, 16)
+    longitud = 12
     caracteres = ascii_letters + digits + punctuation
-    contrasena = ''.join(random.choice(caracteres) for i in range(longitud))
+    contrasena = ''.join(random.choice(caracteres) for _ in range(longitud-1))
+    posicion_coma = random.randint(0, len(contrasena))
+    contrasena = contrasena[:posicion_coma] + ',' + contrasena[posicion_coma:]
+    
     contrasena_entry.delete(0, tk.END)
     contrasena_entry.insert(0, contrasena)
     print("Contraseña generada:", contrasena)
@@ -27,16 +29,13 @@ ventana = tk.Tk()
 ventana.geometry("350x210")
 ventana.title("ClaveGen v1.0")
 
-# Etiqueta de advertencia
 etiqueta_advertencia = tk.Label(ventana, text="Al presionar Salir, la contraseña se eliminará del portapapeles.",
                                 fg="red", font=("Arial", 8))
 etiqueta_advertencia.pack(padx=10, pady=5)
 
-# Entrada para la contraseña
 contrasena_entry = ttk.Entry(ventana, width=30)
 contrasena_entry.pack(padx=10, pady=10)
 
-# Botones
 boton_generar = tk.Button(ventana, text="Generar contraseña", command=generar_contrasena)
 boton_generar.pack(padx=10, pady=10)
 
@@ -46,7 +45,6 @@ boton_copiar.pack(padx=10, pady=10)
 boton_salir = tk.Button(ventana, text="Salir", command=salir)
 boton_salir.pack(padx=10, pady=10)
 
-# Cerrar la aplicación con la "X"
 ventana.protocol("WM_DELETE_WINDOW", salir)
 
 ventana.mainloop()
